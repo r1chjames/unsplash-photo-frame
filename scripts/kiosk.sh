d9e9191a5e9638a2ccf9868f69a274b11b0d7b62
Richard James <richjames11@gmail.com>
2025-11-04 20:32:16 +0000

    Kiosk script improvements

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Wait for the network to be ready
-sleep 15
+# --- Kiosk Script ---
 
-# REQUIRED: Change this to the full path of your app
-cd /home/rich/Documents/unsplash-photo-frame
+# Define the full path to your app directory
+APP_DIR="/home/rich/Documents/unsplash-photo-frame"
 
-# Start the server in the background
-node backend/server.js &
+# Start the node server in the background
+# We will log all output (and errors) to a file for debugging
+node "$APP_DIR/backend/server.js" > "$APP_DIR/scripts/node.log" 2>&1 &
 
-# Wait for the server to start
+# Wait for the server to start (5 seconds is usually safe)
 sleep 5
 
-# Run unclutter in the background to hide the mouse after 1 sec
+# Start unclutter to hide the mouse
 unclutter -idle 1 &
 
-# Launch Chromium
-luakit http://localhost:3000
+# Launch Luakit (the config file we made earlier will make it fullscreen)
+luakit http://localhost:8080
